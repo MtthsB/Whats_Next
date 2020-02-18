235316ecf38ad5b21515b2c0e269e3dbd913a05e
Matthias <matthias@bao-bab.be>
2020-02-18 09:00:36 +0100

    first commit, basic setup

---
+++ next.config.js
@@ -0,0 +1,4 @@
+const withSass = require('@zeit/next-sass') // eslint-disable-line
+module.exports = withSass({
+  /* config options here */
+})
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// styling
+import '../styles/main.scss'
+
+const Home = (): JSX.Element => {
+  return (
+    <div>
+      <h1 className='home__title'>This is the landing page</h1>
+      <h2 className='home__subtitle'>Here you will be able to navigate to all three pages </h2>
+    </div>
+  )
+}
+
+export default Home
