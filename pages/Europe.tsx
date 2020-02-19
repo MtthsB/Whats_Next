0e3c61c0ba32316447849bded9d9dfb2d5cd86d4
Matthias <matthias@bao-bab.be>
2020-02-19 14:22:16 +0100

    2001-2004 implemented

--- pages/Europe.tsx
+++ pages/Europe.tsx
@@ -1,12 +1,96 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-import { Layout } from '../components'
+import * as countryService from '../services/countryService'
+import { Country } from '../models'
+import { Layout, Spinner } from '../components'
 
 // styling
 import '../styles/main.scss'
 
-export default () => {
-  return (
-    <Layout title='The Great Old European Union' />
-  )
+type Props = {
+  countries: Country[];
 }
+
+const Europe = (props: Props): JSX.Element => {
+  const [ countries, updateCountries ] = useState<Country[]>(props.countries.slice(0, 20))
+  const [ loadPosition, updateLoadPosition ] = useState<number>(10)
+  const [isLoading, toggleLoading] = useState<boolean>(true)
+  const [isFetching, toggleFetching] = useState<boolean>(false)
+
+  const scrollOffset = 300
+  const scrollRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll)
+
+    if (props.countries) {
+      // Just fake some initial loading state
+      setTimeout(() => {
+        toggleLoading(false)
+      }, 1000)
+    }
+
+    return () => window.removeEventListener('scroll', onScroll)
+  })
+
+  const onScroll = () => {
+    if ((window.scrollY + window.innerHeight >= scrollRef.current.scrollHeight - scrollOffset) && (countries.length < props.countries.length)) {
+      toggleFetching(true)
+
+      const amountToLoad = (props.countries.length - countries.length < 10) ? (props.countries.length - countries.length) : 10
+
+      // Just fake some loading state
+      setTimeout(() => {
+        updateCountries(countries.concat(props.countries.slice(loadPosition, loadPosition + amountToLoad)))
+        updateLoadPosition(loadPosition + 10)
+        toggleFetching(false)
+      }, 1000)
+    }
+  }
+
+  const renderRow = (country: Country, index: number): JSX.Element => {
+    return (
+      <div className="card" key={`${index}-${country.alpha2Code}`}>
+        <p className="card__row card__row--front">{country.name}</p>
+        <div className="card__row card__row--back">
+          <div className="card__flipside">
+            <p>Name</p>
+            <p>{country.name}</p>
+          </div>
+          <div className="card__flipside">
+            <p>Capital</p>
+            <p>{country.capital}</p>
+          </div>
+          <div className="card__flipside">
+            <p>Alpha2Code</p>
+            <p>{country.alpha2Code}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  if (isLoading) {
+    return (
+      <Layout title="Ye Olde Europe (before Brexit)">
+        <Spinner />
+      </Layout>
+    )
+  } else {
+    return (
+      <Layout title="Ye Olde Europe (before Brexit)">
+        <div className='scrollContainer' ref={scrollRef}>
+          {countries.map(renderRow)}
+          {isFetching && <Spinner />}
+        </div>
+      </Layout>
+    )
+  }
+}
+
+Europe.getInitialProps = async () => {
+  const countries = await countryService.getEUCountries()
+  return { countries }
+}
+
+export default Europe
