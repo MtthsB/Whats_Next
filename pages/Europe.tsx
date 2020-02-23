aa3451d248a34274937f8d6a959f33625edb057a
Matthias <matthias@bao-bab.be>
2020-02-23 13:48:40 +0100

    Added error handling on getInitialProps

--- pages/Europe.tsx
+++ pages/Europe.tsx
@@ -3,26 +3,33 @@ import React from 'react'
 import * as countryService from '../services/countryService'
 import { Country } from '../models'
 import { CountryList } from '../templates'
+import { Layout, Popup } from '../components'
 
 // styling
 import '../styles/main.scss'
-import { Layout } from '../components'
 
 type Props = {
   countries: Country[];
+  hasError: boolean;
 }
 
 const Europe = (props: Props): JSX.Element => {
+  const handleClosePopup = () => window.location.reload()
+
   return (
     <Layout title='Ye Olde Europe (before Brexit)'>
-      <CountryList countries={props.countries} />
+      {props.hasError ? <Popup hasError handleClose={handleClosePopup} /> : <CountryList countries={props.countries} />}
     </Layout>
   )
 }
 
 Europe.getInitialProps = async () => {
-  const countries = await countryService.getEUCountries()
-  return { countries }
+  try {
+    const countries = await countryService.getEUCountries()
+    return { hasError: false, countries }
+  } catch (e) {
+    return { hasError: true, countries: null }
+  }
 }
 
 export default Europe
--- pages/World.tsx
+++ pages/World.tsx
@@ -3,26 +3,33 @@ import React from 'react'
 import * as countryService from '../services/countryService'
 import { Country } from '../models'
 import { CountryList } from '../templates'
+import { Layout, Popup } from '../components'
 
 // styling
 import '../styles/main.scss'
-import { Layout } from '../components'
 
 type Props = {
   countries: Country[];
+  hasError: boolean;
 }
 
 const World = (props: Props): JSX.Element => {
+  const handleClosePopup = () => window.location.reload()
+
   return (
     <Layout title='See the world'>
-      <CountryList countries={props.countries} />
+      {props.hasError ? <Popup hasError handleClose={handleClosePopup} /> : <CountryList countries={props.countries} />}
     </Layout>
   )
 }
 
 World.getInitialProps = async () => {
-  const countries = await countryService.getCountries()
-  return { countries }
+  try {
+    const countries = await countryService.getCountries()
+    return { hasError: false, countries }
+  } catch (e) {
+    return { hasError: true, countries: null }
+  }
 }
 
 export default World
